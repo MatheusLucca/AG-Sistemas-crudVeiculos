c29a89e083f14ebe4f56950a7cc98216a40970c0
MatheusLucca <mat.lualves@gmail.com>
2022-06-11 23:29:16 -0300

    Create base

--- controleDeVeiculos/views.py
+++ controleDeVeiculos/views.py
@@ -3,9 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def inicio(request):
-    return HttpResponse("<h1> Bem vindo ao sistema de controle de veiculos </h1>")
-
-
 def index(request):
     return render(request, 'index.html')
