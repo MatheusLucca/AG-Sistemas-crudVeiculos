5b57d6c67dae3cc429e9468ce81f332d8ba70548
MatheusLucca <mat.lualves@gmail.com>
2022-06-12 18:07:31 -0300

    Create models and register in admin

--- controleDeVeiculos/admin.py
+++ controleDeVeiculos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Controle, Motorista, Veiculo
 
 # Register your models here.
+admin.site.register(Controle)
+admin.site.register(Motorista)
+admin.site.register(Veiculo)
--- controleDeVeiculos/models.py
+++ controleDeVeiculos/models.py
@@ -1,3 +1,43 @@
+
 from django.db import models
 
 # Create your models here.
+
+
+class Veiculo(models.Model):
+    placa = models.CharField(max_length=8)
+    marca = models.CharField(max_length=100)
+    veiculo = models.CharField(max_length=200)
+    km_troca_oleo = models.FloatField(verbose_name='km Troca de óleo')
+
+    def __str__(self):
+        return self.veiculo
+
+
+class Motorista(models.Model):
+    cod_motorista = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=15)
+    cnh = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+
+class Controle(models.Model):
+
+    cod_veiculos = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
+    cod_motorista = models.ForeignKey(Motorista, on_delete=models.CASCADE)
+    data_saida = models.DateField()
+    hora_saida = models.TimeField()
+    km_saida = models.FloatField()
+    destino = models.CharField(max_length=200)
+    data_retorno = models.DateField()
+    hora_retorno = models.TimeField()
+    km_retorno = models.FloatField()
+    km_percorrido = models.FloatField()
+
+    def __str__(self):
+        title = 'Veículo: ' + self.cod_veiculos.veiculo + \
+            ' - ' + 'Motorista: ' + self.cod_motorista.nome
+        return title
